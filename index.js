24d4a24d07134d777fd8d375e9719150adca32a5
Connor Forbes <connorforbes25@gmail.com>
2023-09-26 10:57:42 +1000

    feat: Basic server to send websocket requests

---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+require('dotenv').config();
+
+const domainName = process.env.DOMAIN_NAME;
+
+// Load the Certbot SSL certificate and private key
+const server = https.createServer({
+  cert: fs.readFileSync(`/etc/letsencrypt/live/${domainName}/fullchain.pem`),
+  key: fs.readFileSync(`/etc/letsencrypt/live/${domainName}/privkey.pem`)
+});
+
+// Create websocket server
+const wss = new WebSocket.Server({ server });
+
+// Store all connected clients
+const clients = [];
+
+wss.on('connection', (ws) => {
+  clients.push(ws);
+
+  ws.on('close', () => {
+    const index = clients.indexOf(ws);
+    if (index > -1) {
+      clients.splice(index, 1);
+    }
+  });
+});
+
+// Listen for terminal input
+process.stdin.on('data', (data) => {
+  const command = data.toString().trim();
+
+  // If the 'send' command is entered, send a 'displaySurvey' message to all clients
+  if (command === 'send') {
+    clients.forEach(client => client.send('displaySurvey'));
+    console.log('Sent displaySurvey to all connected clients.');
+  }
+});
+
+// Start the server on port 8080
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
