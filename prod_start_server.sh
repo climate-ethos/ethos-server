3b8f7f746f8488b6067ff23a6a8ef2eabbc4fdaf
Connor Forbes <connorforbes25@gmail.com>
2024-09-16 12:30:00 +1000

    devops: Docker rootless mode

--- prod_start_server.sh
+++ prod_start_server.sh
@@ -1,1 +1,7 @@
+# Check if .env file exists
+if [ ! -f .env ]; then
+  echo ".env file not found! Please create a .env file in the root directory."
+  exit 1
+fi
+
 docker compose up --build -d --restart unless-stopped
